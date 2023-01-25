d57f880d91a58d453f9b7a2c9fedf3eaf7574301
srujana patwari <68182864+srujana-patwari@users.noreply.github.com>
2023-01-25 16:41:39 -0600

    Update streamlit_app.py
    Added Header and Breakfast menu Details

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy diner!')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 and Blueberry Oatmeal')
+streamlit.text('Kale, spinach & Rocket smoothie')
+streamline.text('Hard boiled Free_Range Egg')
